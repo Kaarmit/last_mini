85900f7947a05412276af966a435ee4a9b301da7
Alexis Armitano <aarmitan@made-f0Ar5s4.clusters.42paris.fr>
2025-04-18 10:48:59 +0200

    mise a niveau avec seyf

--- SRCS/CLEAN/clean_utils.c
+++ SRCS/CLEAN/clean_utils.c
@@ -6,103 +6,11 @@
 /*   By: aarmitan <aarmitan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 11:55:23 by aarmitan          #+#    #+#             */
-/*   Updated: 2025/04/17 12:48:55 by aarmitan         ###   ########.fr       */
+/*   Updated: 2025/04/18 10:42:46 by aarmitan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../INCLUDES/minishell.h"
-
-// char	**malloc_free(char	**tab)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	if (!tab)
-// 		return (NULL);
-// 	while (tab[i])
-// 	{
-// 		free(tab[i]);
-// 		i++;
-// 	}
-// 	free(tab);
-// 	tab = NULL;
-// 	return (NULL);
-// }
-
-// void	free_envcpy(char **envcpy)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (envcpy[i])
-// 	{
-// 		free(envcpy[i]);
-// 		i++;
-// 	}
-// 	free(envcpy);
-// }
-
-// void	free_data(t_data *data)
-// {
-// 	if (!data)
-// 		exit(EXIT_SUCCESS);
-// 	if(data->tokens)
-// 		malloc_free(data->tokens);
-// 	if (data->token)
-// 		free_lst(data->token);
-// 	if (data->brut_input)
-// 		free(data->brut_input);
-// 	if(data->envcpy)
-// 		free_envcpy(data->envcpy);
-// 	if(data->chunk)
-// 		lst_clear_chunk(&data->chunk, &free_ptr);
-// 	if(data->env)
-// 		clear_env_lst(&data->env);
-// 	close(data->stdin);
-// 	close(data->stdout);
-// 	free(data);
-// 	exit(EXIT_SUCCESS);
-// }
-
-// void	free_data_bis(t_data *data)
-// {
-// 	if (!data)
-// 	{
-// 		printf("data is NULL\n");
-// 		close(data->stdin);
-// 		close(data->stdout);
-// 		exit(EXIT_SUCCESS);
-// 	}
-// 	if(data->tokens)
-// 		malloc_free(data->tokens);
-// 	if (data->token)
-// 		free_lst(data->token);
-// 	if (data->brut_input)
-// 		free(data->brut_input);
-// 	if(data->chunk)
-// 		lst_clear_chunk(&data->chunk, &free_ptr);
-// }
-
-// void	free_data_exec(t_data *data)
-// {
-// 	if (!data)
-// 	{
-// 		printf("data is NULL\n");
-// 		exit(EXIT_SUCCESS);
-// 	}
-// 	// if (data->brut_input)
-// 	// 	free(data->brut_input);
-// 	if(data->chunk)
-// 		lst_clear_chunk(&data->chunk, &free_ptr);
-// }
-
-
-// void	clean_program(t_data *data)
-// {
-// 	free_data(data);
-// 	// clear_env_lst(&data->env);
-// 	rl_clear_history();
-// }
 
 char    **malloc_free(char **tab)
 {
@@ -171,7 +79,6 @@ void    free_data_exit(t_data *data)
 	close(data->stdin);
 	close(data->stdout);
 	free(data);
-	// exit(EXIT_SUCCESS);
 }
 
 void    free_data_bis(t_data *data)
@@ -200,8 +107,6 @@ void    free_data_exec(t_data *data)
 		printf("data is NULL\n");
 		exit(EXIT_SUCCESS);
 	}
-	// if (data->brut_input)
-	//     free(data->brut_input);
 	if (data->chunk)
 		lst_clear_chunk(&data->chunk, &free_ptr);
 	free_envcpy(data->envcpy);
@@ -209,9 +114,26 @@ void    free_data_exec(t_data *data)
 	free(data);
 }
 
-void    clean_program(t_data *data)
+void	clean_program(t_data *data)
 {
-	free_data(data);
-	// clear_env_lst(&data->env);
+	close(data->stdin);
+	close(data->stdout);
+	if (data->envcpy)
+		free_envcpy(data->envcpy);
+	clear_env_lst(&data->env);
+	lst_clear_chunk(&data->chunk, &free_ptr);
+	free(data);
 	rl_clear_history();
+	exit(0);
+}
+
+void	clean_program_exec(t_data *data)
+{
+	if (data->envcpy)
+		free_envcpy(data->envcpy);
+	clear_env_lst(&data->env);
+	lst_clear_chunk(&data->chunk, &free_ptr);
+	free(data);
+	rl_clear_history();
+	exit(127);
 }
